8473a95dce1da6a90227fdb3918d7f6002c09b17
Curt-Lucas <60093618+Curt-Lucas@users.noreply.github.com>
2022-12-25 00:03:43 -0500

    Update
    Class based arch

---
+++ pricebot2.py
@@ -0,0 +1,50 @@
+import os
+import asyncio
+import logging
+import requests
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+BTC_API_URL = 'https://min-api.cryptocompare.com/data/price?fsym=btc&tsyms=USD'
+ETH_API_URL = 'https://min-api.cryptocompare.com/data/price?fsym=eth&tsyms=USD'
+
+logging.basicConfig(format='%(message)s')
+log = logging.getLogger(__name__)
+        
+def get_latest_crypto_price(crypto):
+    if crypto == 'btc':
+        response = requests.get(BTC_API_URL)
+    elif crypto == 'eth':
+        response = requests.get(ETH_API_URL)
+        
+    response_json = response.json()
+    #return float(response_json[0]['USD'])
+    return response_json['USD'];
+
+class MyClient(discord.Client):
+    
+    async def on_ready(self):
+        print(f'We are ready...')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+        if message.author == self.user:
+            return
+        
+        if message.content == '!btc':
+            price = get_latest_crypto_price('btc')
+            return await message.channel.send(f'Bitcoin (BTC): {price}')
+        elif message.content == '!eth':
+            price = get_latest_crypto_price('eth')
+            return await message.channel.send(f'Ethereum (ETH): {price}')
+            
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(TOKEN)
