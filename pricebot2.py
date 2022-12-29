5e65a653ef9f684b39852554be2eb7c1e5a1d78b
Curt-Lucas <60093618+Curt-Lucas@users.noreply.github.com>
2022-12-28 19:45:32 -0500

    Update pricebot2.py
    Added % change of price since last price request

--- pricebot2.py
+++ pricebot2.py
@@ -12,14 +12,19 @@ ETH_API_URL = 'https://min-api.cryptocompare.com/data/price?fsym=eth&tsyms=USD'
 
 logging.basicConfig(format='%(message)s')
 log = logging.getLogger(__name__)
-        
+
 def get_latest_crypto_price(crypto):
     if crypto == 'btc':
         response = requests.get(BTC_API_URL)
     elif crypto == 'eth':
         response = requests.get(ETH_API_URL)
     response_json = response.json()
-    return response_json['USD'];
+    return response_json['USD']
+    
+def get_change(original, current):
+    change = current - original
+    pct_change = change / original * 100
+    return pct_change
 
 class MyClient(discord.Client):
     
@@ -27,6 +32,8 @@ class MyClient(discord.Client):
         print(f'We are ready...')
 
     async def on_message(self, message):
+        global btc_last_price
+        global eth_last_price
         print(f'Message from {message.author}: {message.content}')
 
         if message.author == self.user:
@@ -34,11 +41,22 @@ class MyClient(discord.Client):
         
         if message.content == '!btc':
             price = get_latest_crypto_price('btc')
-            return await message.channel.send(f'Bitcoin (BTC): {price}')
+            pct_change = str(round(get_change(btc_last_price, price), 2))
+            btc_last_price = price
+            return await message.channel.send(f'Bitcoin (BTC): {price} ({pct_change})')
         elif message.content == '!eth':
             price = get_latest_crypto_price('eth')
-            return await message.channel.send(f'Ethereum (ETH): {price}')
-            
+            pct_change = str(round(get_change(eth_last_price, price), 2))
+            eth_last_price = price
+            return await message.channel.send(f'Ethereum (ETH): {price} ({pct_change})')
+
+
+btc_last_price = float(get_latest_crypto_price('btc'))
+eth_last_price = float(get_latest_crypto_price('eth'))
+
+print(f'Latest BTC: {btc_last_price}')
+print(f'Latest ETH: {eth_last_price}')
+
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
